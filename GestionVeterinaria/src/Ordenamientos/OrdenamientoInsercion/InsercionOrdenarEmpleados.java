aa1c7e8c135c06f1766cd278bf399af0c3a1aaf8
Lorena Garcia <lorenaamesqueta@gmail.com>
2024-05-15 15:45:57 -0500

    subiendo avance
    Subiendo nuevos paquetes

---
+++ GestionVeterinaria/src/Ordenamientos/OrdenamientoInsercion/InsercionOrdenarEmpleados.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ordenamientos.OrdenamientoInsercion;
+
+/**
+ *
+ * @author pc
+ */
+public class InsercionOrdenarEmpleados {
+    
+}
