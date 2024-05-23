8493ff2727a869500d54fb46393e387ee5a13f0b
R4xx1s <cesarftc25@gmail.com>
2024-05-23 15:39:54 -0500

    CLIENTES VET2
    ya no creen más ramas xdd

--- GestionVeterinaria/src/Controlador/ControladorClientes.java
+++ GestionVeterinaria/src/Controlador/ControladorClientes.java
@@ -3,8 +3,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controlador;
-
+import Modelo.Cliente;
+import Modelo.ClienteDAO;
+import Vista.Clientes;
 
 public class ControladorClientes {
-    
+    private ClienteDAO modelo;
+    private Clientes vista;
+
+
+public ControladorClientes(Clientes vista) {
+        this.vista = vista;
+        this.modelo = new ClienteDAO();
+    }
+
+    public void buscarClientePorNombre(String nombre) {
+        Cliente cliente = modelo.buscarPorNombre(nombre);
+        vista.mostrarCliente(cliente);
+    }
+
+    public void buscarClientePorCodigo(String codigo) {
+        Cliente cliente = modelo.buscarPorCodigo(codigo);
+        vista.mostrarCliente(cliente);
+    }
+
+    public void buscarClientePorEdad(int edad) {
+        Cliente cliente = modelo.buscarPorEdad(edad);
+        vista.mostrarCliente(cliente);
+    }
 }
