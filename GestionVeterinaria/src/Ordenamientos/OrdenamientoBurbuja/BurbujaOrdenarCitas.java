fc94bef8307ce63baa91f1228de2f0382515db97
MarcoDrew19 <marcodrewjb@gmail.com>
2024-05-25 18:51:03 -0500

    subiendo otr avance ya no quiero
    asdasd

--- GestionVeterinaria/src/Ordenamientos/OrdenamientoBurbuja/BurbujaOrdenarCitas.java
+++ GestionVeterinaria/src/Ordenamientos/OrdenamientoBurbuja/BurbujaOrdenarCitas.java
@@ -1,18 +1,15 @@
     
 package Ordenamientos.OrdenamientoBurbuja;
-import java.io.*;
-import Modelo.*;
-import Modelo.ArregloCitas;
+
 import Modelo.Citas;
 
 public class BurbujaOrdenarCitas {
-    //ES CORRECTO PONER ESTO?
-    private static int cantCitas=0;
+    
+    
     //TIPO DE URGENCIA**********************************************************
     
-    
-    public static Citas[] OrdenarPorUrgencia( Citas[] ListaCitas ) {
-        for (int i = 0; i < - 1; i++) {
+     public static Citas[] OrdenarPorUrgenciaASC(Citas[] ListaCitas, int cantCitas) {
+        for (int i = 0; i < cantCitas - 1; i++) {
             for (int j = 0; j < cantCitas - 1 - i; j++) {
                 if (ListaCitas[j].getUrgencia().compareTo(ListaCitas[j + 1].getUrgencia()) > 0) {
                     Citas temp = ListaCitas[j];
@@ -23,6 +20,64 @@ public class BurbujaOrdenarCitas {
         }
         return ListaCitas;
     }
+
+    public static Citas[] OrdenarPorUrgenciaDESC(Citas[] ListaCitas, int cantCitas) {
+        for (int i = 0; i < cantCitas - 1; i++) {
+            for (int j = 0; j < cantCitas - 1 - i; j++) {
+                if (ListaCitas[j].getUrgencia().compareTo(ListaCitas[j + 1].getUrgencia()) < 0) {
+                    Citas temp = ListaCitas[j];
+                    ListaCitas[j] = ListaCitas[j + 1];
+                    ListaCitas[j + 1] = temp;
+                }
+            }
+        }
+        return ListaCitas;
+    }
+}
+    
+    /*
+    // Ordenar por urgencia en orden ascendente
+    public static Citas[] OrdenarPorUrgenciaAscendente(Citas[] ListaCitas) {
+        int cantCitas = ListaCitas.length;
+        for (int i = 0; i < cantCitas - 1; i++) {
+            for (int j = 0; j < cantCitas - 1 - i; j++) {
+                if (ListaCitas[j].getUrgencia().compareTo(ListaCitas[j + 1].getUrgencia()) > 0) {
+                    Citas temp = ListaCitas[j];
+                    ListaCitas[j] = ListaCitas[j + 1];
+                    ListaCitas[j + 1] = temp;
+                }
+            }
+        }
+        return ListaCitas;
+    }
+    
+    // Ordenar por urgencia en orden descendente
+    public static Citas[] OrdenarPorUrgenciaDescendente(Citas[] ListaCitas) {
+        int cantCitas = ListaCitas.length;
+        for (int i = 0; i < cantCitas - 1; i++) {
+            for (int j = 0; j < cantCitas - 1 - i; j++) {
+                if (ListaCitas[j].getUrgencia().compareTo(ListaCitas[j + 1].getUrgencia()) < 0) {
+                    Citas temp = ListaCitas[j];
+                    ListaCitas[j] = ListaCitas[j + 1];
+                    ListaCitas[j + 1] = temp;
+                }
+            }
+        }
+        return ListaCitas;
+    }
+    
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
@@ -42,4 +97,4 @@ public class BurbujaOrdenarCitas {
         return arreglo;
     }//fin del método 
 */
-}
+
