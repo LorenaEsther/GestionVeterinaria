1aa4d055922ffa9e45a0d08ddfd770123b14affb
R4xx1s <cesarftc25@gmail.com>
2024-07-01 17:22:44 -0500

    Busqueda Binaria Citas
    volvi a agregar ese que se elimino

---
+++ GestionVeterinaria/src/Busquedas/BusquedaBinaria/BusquedaBinariaCitas.java
@@ -0,0 +1,22 @@
+package Busquedas.BusquedaBinaria;
+
+import Modelo.*;
+public class BusquedaBinariaCitas {
+    /*public static int BuscarPorCodigoCita(Citas[] vector, String codigobuscar) {
+        int n = ArregloCitas.getCantCitas();
+        int inferior = 0, superior = n - 1;
+        while (inferior <= superior) {
+            int centro = (superior + inferior) / 2;
+            if (codigobuscar.equalsIgnoreCase(vector[centro].getIdCita())) {
+                return centro;
+            } else {
+                if (codigobuscar.compareToIgnoreCase(vector[centro].getIdCita()) < 0)
+                    superior = centro - 1;
+                else
+                    inferior = centro + 1;
+            }
+        }
+        return -1;
+    }
+*/
+}
